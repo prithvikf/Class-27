a281b99ac29106abd3d0b9e7bcdfe8c251c90646
prithvikf <prithvikf@gmail.com>
2021-01-16 20:14:55 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var contrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,8 +36,11 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    contrainedLog=new Log(230,180,80,PI/2);
 
+    bird = new Bird(100,100);
+    chain=new Chain(bird.body,contrainedLog.body)
+    
 }
 
 function draw(){
@@ -61,4 +66,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    
+    contrainedLog.display();
+    chain.display();
+    
 }
